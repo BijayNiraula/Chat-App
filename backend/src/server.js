78345e454f1b071bbed6804eda91a9647271a775
bijay niraula <bijayniraula7@gmail.com>
2024-04-26 14:43:38 +0545

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,68 @@
+const express=require("express");
+const{Server}=require('socket.io');
+const {createServer}=require("http");
+const cors=require("cors")
+const connectDb=require("./Db/connectDb")
+const Message=require("./model/message")
+const app=express();
+
+
+app.use(express.json())
+const port=process.env.port ||8000;
+  
+app.use(cors());
+const server=createServer(app);
+const io=new Server(server,{
+<<<<<<< HEAD
+    cors:"https://chattu.netlify.app/",
+=======
+    cors:"https://chattu.netlify.app",
+>>>>>>> 67666eeae1885f483b763b1bba243e702224f45b
+    methods:["GET","POST"],
+},)
+
+io.on("connection",(socket)=>{
+    console.log("connected successfully ")
+
+    socket.on('join',(data)=>{ 
+        console.log("joined successfully")
+        socket.join(data.id)
+        socket.to(data.id).emit("join_info",data.email)
+        socket.on("send_message",async(message)=>{
+          socket.to(data.id).emit("receive_message",message);
+         await Message.create({name:message.name,email:message.email,message:message.message,id:data.id})
+      })
+})})
+
+
+
+
+app.post("/",async(req,res)=>{
+    console.log(req.body)
+     const id= req.body.id;
+    const data=await Message.find({id});
+    res.send(data);
+
+})
+
+
+
+
+const start=async()=>{
+    try{
+        await connectDb()
+        server.listen(port,()=>{
+            console.log("server is running in port "+port);
+        })
+    }catch(e){
+        console.log(e)
+    }
+   
+}
+start();
+
+
+
+
+
+
